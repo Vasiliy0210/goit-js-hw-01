29b3666b7e2376c8d9e8a54939aa1853d3110905
Vasiliy <gnativ.vasiliy@gmail.com>
2019-11-27 00:16:28 +0200

    1st hw added 2711

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,16 @@
 // ------------------task-06
 
-
 let input;
 let total = 0;
 
 do {
     input = prompt('Please enter your number');
-    total = Number(input) + total;
+    if (isNaN(input)) {
+        alert('Enter number please')
+    } else {
+        total = Number(input) + total;
+    }
+
 } while (input !== null);
 
 
